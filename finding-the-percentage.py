ee82daad4819061bb095428de783d1189416a0fb
Yeison Villegas <yvillegas@plm.com.co>
2024-02-08 15:04:51 -0500

    fix finding percentage

--- finding-the-percentage.py
+++ finding-the-percentage.py
@@ -8,16 +8,13 @@ def findStudentScore(students, studentName):
             return students[i]
 
 if __name__ == '__main__':
-    """ n = int(input())
+    n = int(input())
     student_marks = {}
     for _ in range(n):
         name, *line = input().split()
         scores = list(map(float, line))
         student_marks[name] = scores
-    query_name = input() """
-    n = 3
-    student_marks = {'Krishna': [67.0, 68.0, 69.0], 'Arjun': [70.0, 98.0, 63.0], 'Malika': [52.0, 56.0, 60.0]}
-    query_name = 'Malika'
+    query_name = input()
 
     # Buscar las notas del estudiante
     scores = findStudentScore(student_marks, query_name)
