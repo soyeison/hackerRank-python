288f508c7798badf0ae7d09fa9966c976b4af435
Yeison Villegas <yvillegas@plm.com.co>
2024-02-21 09:12:13 -0500

    div-mod solucionado

---
+++ mod-divmod.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    # Integer division
+    print(a // b)
+
+    # Result of module operator
+    print(a % b)
+
+    # divmod operation
+    print(divmod(a, b))
